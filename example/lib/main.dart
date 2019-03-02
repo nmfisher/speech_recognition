13027df49025bb7bfe7bfdca28e6e65dc57d3452
Nick Fisher <nick.fisher@avinium.com>
2019-03-02 20:41:08 +1100

    revert

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -152,7 +152,7 @@ class _MyAppState extends State<MyApp> {
 
   void onRecognitionResult(String text) => setState(() => transcription = text);
 
-  void onRecognitionComplete() => setState(() => _isListening = false);
+  void onRecognitionComplete(String foo) => setState(() => _isListening = false);
 
   void errorHandler() => print("error");
 }
